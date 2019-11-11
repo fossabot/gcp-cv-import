e29ac5dff082a7535a316576682014574afde0a5
Matt Riley <matt.riley@metrisk.com>
2019-11-11 11:26:31 +0000

    Fixed test

--- __test__/ImportProductSet.test.ts
+++ __test__/ImportProductSet.test.ts
@@ -6,29 +6,29 @@ describe('ImportProductSet', () => {
       const subject = new ImportProductSet({
         projectId: 'Foo',
         location: 'Bar',
-        csvUri: 'Baz',
-      })
-      expect(subject["options"].projectId).toEqual('Foo'); 
-      expect(subject["options"].location).toEqual('Bar');
-      expect(subject["options"].csvUri).toEqual('Baz');
-      expect(subject["options"].logger).toEqual(console);
-    })
+        csvUri: 'Baz'
+      });
+      expect(subject['options'].projectId).toEqual('Foo');
+      expect(subject['options'].location).toEqual('Bar');
+      expect(subject['options'].csvUri).toEqual('Baz');
+      expect(subject['options'].logger).toEqual(console);
+    });
     test('options are correctly set with a custom logger', () => {
       const customLogger = {
         info: console.info
-      }
+      };
       const subject = new ImportProductSet({
         projectId: 'Foo',
         location: 'Bar',
-        csvUri: "Baz",
+        csvUri: 'Baz',
         logger: customLogger
-      })
-      expect(subject["options"].projectId).toEqual('Foo')
-      expect(subject['options'].location).toEqual('Bar')
-      expect(subject['options'].csvUri).toEqual('Baz')
-      expect(subject['options'].logger).toEqual(customLogger)
-    })
-  })
+      });
+      expect(subject['options'].projectId).toEqual('Foo');
+      expect(subject['options'].location).toEqual('Bar');
+      expect(subject['options'].csvUri).toEqual('Baz');
+      expect(subject['options'].logger).toEqual(customLogger);
+    });
+  });
   describe('import', () => {
     let subject: ImportProductSet;
     beforeEach(() => {
@@ -36,17 +36,17 @@ describe('ImportProductSet', () => {
         projectId: 'Foo',
         location: 'Bar',
         csvUri: 'Baz'
-      })
+      });
 
       subject.import();
-    })
+    });
     test('function to have called cloud visions locationPath', () => {
-      expect(subject["client"].locationPath).toHaveBeenCalledWith({
+      expect(subject['client'].locationPath).toHaveBeenCalledWith({
         projectId: 'Foo',
         location: 'Bar'
-      })
+      });
       expect(subject['client'].locationPath).toHaveReturned();
-    })
+    });
     test('function to have called cloud visions importProductSets', () => {
       expect(subject['client'].importProductSets).toHaveBeenCalledWith({
         inputConfig: {
@@ -54,8 +54,8 @@ describe('ImportProductSet', () => {
             csvFileUri: 'Baz'
           }
         }
-      })
-      expect(subject['client'].importProductSets).toHaveReturned()
-    })
-  })
-})
+      });
+      expect(subject['client'].importProductSets).toHaveReturned();
+    });
+  });
+});
